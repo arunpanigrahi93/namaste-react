648d196723b7581119f6710c7c55f5b04a367ad5
arunpanigrahi93 <arunpanigrahi94@gmail.com>
2025-06-18 12:50:39 +0530

    remove cdn and instal npm for react and react dom

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client"; // use correct import for React 18+
+
 const heading = React.createElement(
   "div",
   { id: "parent" },
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "im from h1"),
-    React.createElement("h2", {}, "im from h2"),
+    React.createElement("h1", { key: "h1" }, "I'm from h1"),
+    React.createElement("h2", { key: "h2" }, "I'm from h2"),
   ])
 );
 
