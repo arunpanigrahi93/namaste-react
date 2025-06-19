7f0c3eca7d6dbc5154bac3683dc52f6c89990a40
arunpanigrahi93 <arunpanigrahi94@gmail.com>
2025-06-19 17:59:53 +0530

    body

--- App.js
+++ App.js
@@ -22,10 +22,39 @@ const Header = () => {
   );
 };
 
+const RestaurantCard = ({ resName, cusine }) => {
+  return (
+    <div className="res-card">
+      <img
+        className="res-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/12/9/ed859a04-8f52-434b-aca9-6de31d0247fd_95228.JPG"
+        alt="img"
+      />
+      <h3>{resName}</h3>
+      <h4>{cusine}</h4>
+      <h4>4.4 stars</h4>
+      <h4>38 minits</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard resName="Meghana Foods" cusine="Biryani, NorthIndian" />
+        <RestaurantCard resName="Kfc" cusine="south indian" />
+      </div>
+    </div>
+  );
+};
+
 const AppLayout = () => {
   return (
     <div>
       <Header />
+      <Body />
     </div>
   );
 };
