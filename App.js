62645dd5c7bfa0da707b6652749a64cd0a213020
arunpanigrahi93 <arunpanigrahi94@gmail.com>
2025-06-19 16:39:10 +0530

    header

--- App.js
+++ App.js
@@ -1,24 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom/client"; // use correct import for React 18+
 
-//react element to convert functional component
-//const heading = <h1>Hello</h1>
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://static.vecteezy.com/system/resources/previews/009/291/628/original/restaurant-logo-design-vector.jpg"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-// component
-const Heading = () => <h1>Hello </h1>;
-
-//component
-
-const HeadingComponent = () => {
+const AppLayout = () => {
   return (
     <div>
-      <Heading />
-      <h1>Hello from functional component</h1>
+      <Header />
     </div>
   );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
 
 //component inside other component is called Component Composition
